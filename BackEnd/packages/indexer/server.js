2ba5d5af4a3b690b338ba8d6cd82ce1aa6e9f53d
TGBinh <truonggiabinh1231@gmail.com>
2025-03-21 15:27:17 +0700

    feat: add file server.js

---
+++ BackEnd/packages/indexer/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: 'http://localhost:4000',
+    changeOrigin: true,
+  })
+);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Wrapper server listening on port ${port}`);
+});
